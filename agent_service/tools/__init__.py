777ecd29cf48e06583cb754005d77ae59a5921f9
Daniel Shatzov <shatzovdani@gmail.com>
2026-09-22 18:01:51 +0300

    feat: add API execution and log inspection tools

--- agent_service/tools/__init__.py
+++ agent_service/tools/__init__.py
@@ -1,19 +1,41 @@
 """Narrow, application-controlled Sandbox investigation tools."""
 
 from agent_service.tools.contracts import HttpMethod, ToolErrorType, ToolResult
+from agent_service.tools.execute_api_request import (
+    ExecuteApiRequestArgs,
+    ExecuteApiRequestData,
+    ExecuteApiRequestResult,
+    execute_api_request,
+)
 from agent_service.tools.inspect_api_spec import (
     InspectApiSpecArgs,
     InspectApiSpecData,
     InspectApiSpecResult,
     inspect_api_spec,
 )
+from agent_service.tools.inspect_server_logs import (
+    InspectServerLogsArgs,
+    InspectServerLogsData,
+    InspectServerLogsResult,
+    ServerLogEvent,
+    inspect_server_logs,
+)
 
 __all__ = [
+    "ExecuteApiRequestArgs",
+    "ExecuteApiRequestData",
+    "ExecuteApiRequestResult",
     "HttpMethod",
     "InspectApiSpecArgs",
     "InspectApiSpecData",
     "InspectApiSpecResult",
+    "InspectServerLogsArgs",
+    "InspectServerLogsData",
+    "InspectServerLogsResult",
+    "ServerLogEvent",
     "ToolErrorType",
     "ToolResult",
+    "execute_api_request",
     "inspect_api_spec",
+    "inspect_server_logs",
 ]
